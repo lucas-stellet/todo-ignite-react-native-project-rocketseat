0d48175880999d367814d8546c1cf225cc3531c9
Lucas Stellet <lustepe@gmail.com>
2022-08-22 10:33:23 -0300

    refactora o projeto adicionando styed-components

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,26 +1,82 @@
-import { Image, TouchableOpacity, TextInput, View, Text } from 'react-native';
-import { styles } from './styles';
+import React from 'react';
+import {
+  Image,
+  TouchableOpacity,
+  TextInput,
+  View,
+  Text,
+  FlatList,
+} from 'react-native';
+import {
+  Container,
+  Header,
+  Form,
+  TaskInput,
+  CreateTaskButton,
+  TasksHeader,
+  Content,
+  TasksHeaderGroup,
+  CreatedTasksTitle,
+  DoneTasksTitle,
+  TasksHeaderBadge,
+  TasksHeaderAmount,
+  TasksList,
+  NoTasks,
+  NoTasksTitle,
+  NoTasksSubTitle,
+} from './styles';
 
 const logo = require('./../../../assets/logo.png');
 const plus = require('./../../../assets/plus.png');
+const clipboard = require('../../../assets/clipboard.png');
 
 export function Home() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
+    <Container>
+      <Header>
         <Image source={logo} />
-      </View>
-      <View style={styles.form}>
-        <TextInput
-          style={styles.input}
+      </Header>
+      <Form>
+        <TaskInput
           placeholder='Adicione uma nova tarefa'
           placeholderTextColor='#808080'
         />
-        <TouchableOpacity style={styles.newTaskButton}>
+        <CreateTaskButton>
           <Image source={plus} />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.content}></View>
-    </View>
+        </CreateTaskButton>
+      </Form>
+      <Content>
+        <TasksHeader>
+          <TasksHeaderGroup>
+            <CreatedTasksTitle>Criadas</CreatedTasksTitle>
+            <TasksHeaderBadge>
+              <TasksHeaderAmount>5</TasksHeaderAmount>
+            </TasksHeaderBadge>
+          </TasksHeaderGroup>
+          <TasksHeaderGroup>
+            <DoneTasksTitle>Concluídas</DoneTasksTitle>
+            <TasksHeaderBadge>
+              <TasksHeaderAmount>5</TasksHeaderAmount>
+            </TasksHeaderBadge>
+          </TasksHeaderGroup>
+        </TasksHeader>
+
+        <FlatList
+          ListEmptyComponent={() => (
+            <NoTasks>
+              <Image source={clipboard} />
+
+              <NoTasksTitle>
+                Você ainda não tem tarefas cadastradas
+              </NoTasksTitle>
+
+              <NoTasksSubTitle>
+                Crie tarefas e organize seus itens a fazer
+              </NoTasksSubTitle>
+            </NoTasks>
+          )}
+        />
+      </Content>
+    </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,42 +1,111 @@
 import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#191919',
-  },
-  header: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#000',
-    maxHeight: 173,
-    padding: 24,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  form: {
-    flexDirection: 'row',
-    marginLeft: 10,
-    marginRight: 10,
-    top: -25,
-  },
-  input: {
-    flex: 1,
-    color: '##F2F2F2',
-    backgroundColor: '#262626',
-    padding: 16,
-  },
-  newTaskButton: {
-    width: 52,
-    height: 52,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#1E6F9F',
-    borderRadius: 10,
-  },
-  content: {
-    flex: 1,
-    backgroundColor: '#191919',
-  },
-});
+import styled, { css } from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #191919;
+  flex-direction: column;
+`;
+
+export const Header = styled.View`
+  flex: 1;
+  flex-direction: row;
+  background-color: #000;
+  max-height: 173px;
+  padding: 24px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  margin-left: 24px;
+  margin-right: 24px;
+  top: -25px;
+`;
+
+export const TaskInput = styled.TextInput`
+  flex: 1;
+  color: #f2f2f2;
+  background-color: #262626;
+  padding: 16px;
+  height: 52px;
+  border-radius: 10px;
+`;
+
+export const CreateTaskButton = styled.TouchableOpacity`
+  width: 52px;
+  height: 52px;
+  align-items: center;
+  justify-content: center;
+  background-color: #1e6f9f;
+  border-radius: 10px;
+  margin-left: 5px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  background-color: #191919;
+  margin: 0px 24px;
+`;
+
+export const TasksHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  width: 327px;
+  height: 19px;
+  margin-bottom: 20px;
+`;
+
+export const TasksHeaderGroup = styled.View`
+  flex-direction: row;
+`;
+
+export const CreatedTasksTitle = styled.Text`
+  color: #4ea8de;
+  font-weight: 700;
+  font-size: 14px;
+`;
+
+export const DoneTasksTitle = styled.Text`
+  color: #8284fb;
+  font-weight: 700;
+  font-size: 14px;
+`;
+
+export const TasksHeaderBadge = styled.View`
+  padding: 2px 8px;
+  margin-left: 8px;
+  border-radius: 999px;
+  width: 25px;
+  height: 19px;
+  background-color: #333333;
+`;
+
+export const TasksHeaderAmount = styled.Text`
+  color: #fff;
+  font-weight: 700;
+  font-size: 13px;
+`;
+
+export const NoTasks = styled.View`
+  padding: 48px 20px;
+  align-items: center;
+  border-style: solid;
+  border-top-width: 1px;
+  border-color: #333333;
+`;
+
+export const NoTasksTitle = styled.Text`
+  margin-top: 16px;
+  font-size: 14px;
+  line-height: 19.6px;
+  color: #808080;
+`;
+
+export const NoTasksSubTitle = styled.Text`
+  font-size: 14px;
+  line-height: 19.6px;
+  color: #808080;
+`;
